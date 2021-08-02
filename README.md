cd60cc7fcf911a075ee61c7f1d115c82fe5fb958
Uthpala Liyanage <uthpalaliyanage@yahoo.com>
2021-08-02 23:07:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HAIVY-BMS
+ Bakery Management System for HAIVY Pastry Shop
---
+++ src/view/AppHome/Main.java
@@ -0,0 +1,27 @@
+package view.AppHome;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setScene(new Scene(root, 1360, 768));
+        primaryStage.setTitle("BAKERY MANAGEMENT SYSTEM - HAIVY PASTRY SHOP");
+        primaryStage.getIcons().add(new Image("/lib/img/haivy-icon.png"));
+        primaryStage.sizeToScene();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

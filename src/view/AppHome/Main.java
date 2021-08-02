b8da8ad314a593ff8858abb8ecaf412c5e3886ea
Uthpala Liyanage <uthpalaliyanage@yahoo.com>
2021-08-02 23:57:35 +0530

    HAIVY-BMS ML - Initial Commit
    Base Size Update to 1360x768 under hide taskbar

--- src/view/AppHome/Main.java
+++ src/view/AppHome/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setScene(new Scene(root, 1360, 768));
+        primaryStage.setScene(new Scene(root, 1360, 730));
         primaryStage.setTitle("BAKERY MANAGEMENT SYSTEM - HAIVY PASTRY SHOP");
         primaryStage.getIcons().add(new Image("/lib/img/haivy-icon.png"));
         primaryStage.sizeToScene();
